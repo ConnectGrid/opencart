d452e4fa5d1f210c055aecbb8e11b3ecb4403cb8
The Digital Orchard <webmaster@thedigitalorchard.ca>
2020-05-16 22:48:26 -0700

    tweaks

--- upload/install/index.php
+++ upload/install/index.php
@@ -14,25 +14,28 @@ if ((isset($_SERVER['HTTPS']) && (($_SERVER['HTTPS'] == 'on') || ($_SERVER['HTTP
 define('HTTP_SERVER',   $protocol . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['SCRIPT_NAME']), '/.\\') . '/');
 define('HTTP_OPENCART', $protocol . $_SERVER['HTTP_HOST'] . rtrim(rtrim(dirname($_SERVER['SCRIPT_NAME']), 'install'), '/.\\') . '/');
 
-define ('OPENCART_CONFIG_FILE',       "{$_SERVER['DOCUMENT_ROOT']}/../opencart-config.php");
-define ('OPENCART_ADMIN_CONFIG_FILE', "{$_SERVER['DOCUMENT_ROOT']}/../opencart-admin-config.php");
+define ('OPENCART_CONFIG_FILE',       realpath("{$_SERVER['DOCUMENT_ROOT']}/../opencart/config.php"));
+define ('OPENCART_ADMIN_CONFIG_FILE', realpath("{$_SERVER['DOCUMENT_ROOT']}/../opencart/admin-config.php"));
 
 // DIR
 define('DIR_OPENCART',     dirname(__DIR__).'/');       //str_replace('\\', '/', realpath(dirname(__FILE__) . '/../') . '/'));
 define('DIR_APPLICATION',  __DIR__.'/');                //str_replace('\\', '/', realpath(dirname(__FILE__))) . '/');
 define('DIR_SYSTEM',       dirname(DIR_APPLICATION).'/system/');  // str_replace('\\', '/', realpath(dirname(__FILE__) . '/../')) . '/system/');
-define('DIR_STORAGE',      realpath("{$_SERVER['DOCUMENT_ROOT']}/../opencart-storage").'/');    // DIR_SYSTEM.'storage/');
-define('DIR_IMAGE',        realpath(DIR_STORAGE.'../opencart-image').'/');                      // str_replace('\\', '/', realpath(dirname(__FILE__) . '/../')) . '/image/');
+define('DIR_STORAGE',      realpath("{$_SERVER['DOCUMENT_ROOT']}/../opencart/storage").'/');    // DIR_SYSTEM.'storage/');
+define('DIR_IMAGE',        realpath(DIR_STORAGE.'../opencart/image').'/');                      // str_replace('\\', '/', realpath(dirname(__FILE__) . '/../')) . '/image/');
+// view
 define('DIR_LANGUAGE',     DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE',     DIR_APPLICATION . 'view/template/');
-define('DIR_DATABASE',     DIR_SYSTEM . 'database/');
-define('DIR_CONFIG',       DIR_SYSTEM . 'config/');
-define('DIR_CACHE',        DIR_STORAGE.'cache/');        // DIR_SYSTEM . 'storage/cache/');
-define('DIR_LOGS',         DIR_STORAGE.'logs/');         // DIR_SYSTEM . 'storage/logs/');
-define('DIR_MODIFICATION', DIR_STORAGE.'modification/'); // DIR_SYSTEM . 'storage/modification/');
-define('DIR_DOWNLOAD',     DIR_STORAGE.'download/');     // DIR_SYSTEM . 'storage/download/');
-define('DIR_SESSION',      DIR_STORAGE.'session/');      // DIR_SYSTEM . 'storage/session/');
-define('DIR_UPLOAD',       DIR_STORAGE.'upload/');       // DIR_SYSTEM . 'storage/upload/');
+// system
+define('DIR_DATABASE',     DIR_SYSTEM  . 'database/');
+define('DIR_CONFIG',       DIR_SYSTEM  . 'config/');
+// storage
+define('DIR_CACHE',        DIR_STORAGE . 'cache/');        // DIR_SYSTEM . 'storage/cache/');
+define('DIR_LOGS',         DIR_STORAGE . 'logs/');         // DIR_SYSTEM . 'storage/logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/'); // DIR_SYSTEM . 'storage/modification/');
+define('DIR_DOWNLOAD',     DIR_STORAGE . 'download/');     // DIR_SYSTEM . 'storage/download/');
+define('DIR_SESSION',      DIR_STORAGE . 'session/');      // DIR_SYSTEM . 'storage/session/');
+define('DIR_UPLOAD',       DIR_STORAGE . 'upload/');       // DIR_SYSTEM . 'storage/upload/');
 
 // Startup
 require_once DIR_SYSTEM . 'startup.php';
